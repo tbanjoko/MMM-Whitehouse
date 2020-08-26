2ea2630faceb3b8db52bf035472d564aefa171c6
tbanjoko <61639839+tbanjoko@users.noreply.github.com>
2020-08-26 12:57:05 -0500

    Add files via upload

---
+++ MMM-Whitehouse.js
@@ -0,0 +1,26 @@
+/// MMM-Whitehouse.js - Core Module
+
+Module.register("MMM-Whitehouse", {
+  getDom: function() {
+    var e = document.createElement("div")
+    e.id = "DISPLAY"
+    return e
+  },
+  notificationReceived: function(notification, payload, sender) {
+    switch(notification) {
+      case "DOM_OBJECTS_CREATED":
+        var timer = setInterval(()=>{
+          this.sendSocketNotification("GIVE_ME_DATA")
+        }, 1000)
+        break
+    }
+  },
+  socketNotificationReceived: function(notification, payload) {
+    switch(notification) {
+      case "HERE_IS_DATA":
+        var e = document.getElementById("DISPLAY")
+        e.innerHTML = payload
+        break
+    }
+  },
+})
