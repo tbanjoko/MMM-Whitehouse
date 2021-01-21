bfde1c523b661cae9b6ad93221d183b153425f81
tbanjoko <61639839+tbanjoko@users.noreply.github.com>
2021-01-21 16:54:22 -0600

    Update realTester.py

--- realTester.py
+++ realTester.py
@@ -2,7 +2,7 @@
 from bs4 import BeautifulSoup                                             
 import requests                                                           
 
-
+#Python3 Parser by Tami Banjoko
 url=requests.get("https://www.whitehouse.gov/presidential-actions/")      
 src=url.content                                                           
 soup = BeautifulSoup(src, 'lxml')
